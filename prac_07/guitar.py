1ff33e8cc1d2b813f66c3d6e4a182a819a16a3cc
NangLaoPha <nanglaopha@my.jcu.edu.au>
2024-11-10 16:48:27 +0800

    Modifing the guitar.py by adding __lt__ and Creating the myguitar.py to test it

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -7,9 +7,7 @@ Actual: 22 minutes
 CURRENT_YEAR = 2024
 VINTAGE_AGE = 50
 
-
 class Guitar:
-
     def __init__(self, name='', year=0, cost=0):
         self.name = name
         self.year = year
@@ -23,3 +21,6 @@ class Guitar:
 
     def is_vintage(self):
         return self.get_age() >= VINTAGE_AGE
+
+    def __lt__(self, other):
+        return self.year < other.year
