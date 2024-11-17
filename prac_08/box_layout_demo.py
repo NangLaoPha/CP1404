166c84c7ea62c4216e9b891cdf664299735158d7
NangLaoPha <nanglaopha@my.jcu.edu.au>
2024-11-17 16:04:18 +0800

    Modifying and Creating the functions for box layout

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -8,5 +8,17 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        """Handle the greeting button press."""
+        # Use the correct 'text' attribute
+        input_text = self.root.ids.input_name.text
+        self.root.ids.output_label.text = f"Hello {input_text}"
+
+    def handle_clear(self):
+        """Clear the text input field and output label."""
+        # Update the 'text' attributes instead of overwriting the widgets
+        self.root.ids.input_name.text = ''
+        self.root.ids.output_label.text = ''
+
 
 BoxLayoutDemo().run()
